d21f55a6daab5b953c46378958f1f7a89cff8cb2
Maeriel Benedicto <N00150817@student.iadt.ie>
2020-12-14 00:03:16 +0000

    Add desc for rotating circle pattern

--- Exercise 02 - Rotating circle pattern/sketch.js
+++ Exercise 02 - Rotating circle pattern/sketch.js
@@ -3,7 +3,6 @@ let rects = [];
 let numRect = 20;
 let rSize;
 
-
 function setup() {
   createCanvas(700, 600);
   // colorMode(RGB, numRect, numRect, numRect);
@@ -11,25 +10,39 @@ function setup() {
 
   background(255);
   // noLoop();
-  rSize =  width/numRect;
-  for (let i = 0; i < numRect; i++) {
-    for (let j = 0; j < numRect; j++) {
-      rects.push(new Rectangle(i, j));
-    }
-  }
 }
 
 function draw() {
   background(0);
+  rSize = width / numRect;
+  for (let i = 0; i < numRect; i++) {
+    for (let j = 0; j < numRect; j++) {
+      push();
+      let transValueX = i * rSize + rSize / 2;
+      let transValueY = j * rSize + rSize / 2;
 
-  rectMode(CENTER);
-  push();
+      let startColor = color(215, 165, 218);
+      let endColor = color(75, 205, 245);
 
-  for (let i = 0; i < rects.length; i++) {
-    push();
-    rects[i].render();
+      translate(transValueX, transValueY);
+      let t = atan2(mouseY - transValueY, mouseX - transValueX);
+      rotate(t);
+      let col = lerpColor(startColor, endColor, (i + j * numRect) / width);
+      fill(col);
+
+      noFill();
+      stroke(i, 0, numRect - j);
+      let d = dist(mouseX, mouseY, transValueX, transValueY);
+      let s = map(d, 0, width, 1, 10, true);
+
+      ellipse(0, 0, rSize / 2, rSize / 2);
+      noStroke();
+      fill(col);
+      ellipse(10, 0, rSize / s, rSize / s);
+      pop();
+    }
   }
-  pop();
+
 
 }
 
